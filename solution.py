6209f01d48f912229f13f91d091a21324a25aaa4
ChrisBueno <ceb587@nyu.edu>
2020-10-10 19:34:04 -0400

    Newer

--- solution.py
+++ solution.py
@@ -77,7 +77,7 @@ def smtp_client(port=1025, mailserver= '127.0.0.1'):
     clientSocket.send(quitCmd.encode())
     recv1=clientSocket.recv(1024).decode()
     #print(recv5)
-    # clientSocket.close()
+    clientSocket.close()
     # Fill in end
 
 
